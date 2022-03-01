1563e214533500ec66432e957b684890069dfb28
EC2 Default User <yuu.wn3@gmail.com>
2022-03-01 10:09:13 +0000

    [Fix]レイアウト調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image, :email)
+    params.require(:user).permit(:name, :profile_image, :email, :introduction)
   end
 
 end
